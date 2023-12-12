93f103fd1d1e25c315b131511c6d6ff55bb2812e
JulioRobelly <julitoto777@gmail.com>
2023-12-12 09:44:14 -0500

    a

---
+++ Trabajo12-12-2023.py
@@ -0,0 +1,26 @@
+#El Proposito de este programa es que podamos practicar la programacion en Pyhtom.
+#Ejercicio1.
+Suma = 4 + 4 #Se suman
+Resta = 4.4 - 2.2 #Se restan
+Multiplicacion = 4 * 4 #Se multiplican
+Division = 8.8 / 2.2 #Se Dividen
+
+print("El resultado de la Suma entre 4+4 es:", Suma)
+print("El resultado de la Resta entre 4.4 - 2.2 es", Resta)
+print("El resultado de la Multiplicacion de 4 * 4 es", Multiplicacion)
+print("El resultado de la Division entre 8.8 / 2.2 es", Division)
+
+#Ejercicio2.
+N = "JulioCesarRobellyRosero"
+Long = len(N) #Con len obtenemos la longitud completa de aquello que pongamos, en este caso, el nombre
+print(N, Long)
+#El proposito de este programa es poder contabilizar los catacteres que puede llegar a tener una variable.
+
+#Ejercicio3.
+V1 = 2
+V2 = 2.2
+R = float(V1) #Transforma de entero a flotante
+R2 = int(V2) #transforma de flotante a entero
+print(V1, R)
+print(V2, R2)
+#El proposito de este programa es realizar la convercion de variables enteras a puntos flotantes y viceversa
